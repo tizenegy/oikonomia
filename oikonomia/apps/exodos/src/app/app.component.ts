b43ec329a7f9c71150d608fca044f87c9ac0bb55
tizenegy <bastian.elfert@outlook.com>
2021-12-11 17:32:38 +0100

    undocumented | BE | created workspace via npm init nx-workspace (yarn did not work)

---
+++ oikonomia/apps/exodos/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'oikonomia-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  title = 'exodos';
+}
